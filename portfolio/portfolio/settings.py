15491ee3f74d7376d5269d179732352f73506c8f
Steve Smith <smiths917@gmail.com>
2021-11-21 18:40:25 -0600

    refactor: changed settings back to use .env file for SECRET_KEY

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# From .env file:
-# from decouple import config
-# SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+## From .env file:
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
+## From environment variable 'SECRET_KEY':
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
