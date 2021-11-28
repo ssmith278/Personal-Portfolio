ee1caeffc55c40d929a1174b5dc07d59b2e22b89
Steve Smith <smiths917@gmail.com>
2021-11-28 06:59:37 -0600

    testing: changed default email backend to print to console to test sending emails

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ADMINS = [('Steve', 'smiths917@gmail.com')]
